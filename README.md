49ac4f1c3e996d98259a28a768ec3eb937e0a131
Yora Li <yorali@Yoras-MacBook-Air.local>
2017-09-04 11:26:19 -0500

    Bot created

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Discord-Bot-JS
+
+Discord Developer Page: https://discordapp.com/developers/
+
+Discord Permission Calculator:  https://discordapi.com/permissions.html
---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+// Bot token goes here
+// ボットのtokenをここに置く
+bot.login('');
+
+console.log('Working');
